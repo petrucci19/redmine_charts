210c0e3102765287792c94303df24729c309cfd3
Ronit <ronit.galani@gritinnovation.com>
2013-02-06 18:22:18 +0530

    minor fixes

--- app/assets/javascripts/project/project_pie.js
+++ app/assets/javascripts/project/project_pie.js
@@ -1,6 +1,4 @@
-
 var plot1, plot2;
-
 
 function dataRenderer(){
   jQuery.ajax( {
@@ -9,8 +7,6 @@ function dataRenderer(){
               //async:false,
               dataType: "json",
               success:  function(data){ 
-                             
-                //console.log(data);
                 txt = "<p>" + data['ns'] + " Test cases not started </br>" + data['p'] + " Test cases passed </br>" + data['f'] + " Test cases failed </br> </p>";
                 document.getElementById("summary_data").innerHTML=txt;
                 //piedata = [  ['Not started',data['ns']], ['failed',data['f']] , ['started',data['p']]  ];
@@ -18,8 +14,6 @@ function dataRenderer(){
                 plot1.series[0].data=  [ ['Design & Analysis',data['ns']], ['Bug-Fixing',data['f']] , ['Development',data['p']] ] ;
                 console.log(plot1.data);  
                 plot1.redraw();
-                //alert('test!');
-
               },
 
               error : function(XMLHttpRequest, textStatus, errorThrown) { console.log('Error!'); } 
@@ -31,17 +25,9 @@ function dataRenderer(){
               url: '/project/list/2',
               dataType: "json",
               success:  function(data){ 
-                
-                //console.log(data);
-                //txt = "<p>" + data['ns'] + " Test cases not started </br>" + data['p'] + " Test cases passed </br>" + data['f'] + " Test cases failed </br> </p>";
-                //document.getElementById("summary_data").innerHTML=txt;
-                //piedata = [  ['Not started',data['ns']], ['failed',data['f']] , ['started',data['p']]  ];
-                //console.log(piedata1);
                 plot2.series[0].data= [ ['Design & Analysis',data['ns']], ['Bug-Fixing',data['f']] , ['Development',data['p']] ];
                 console.log(plot2.data);  
                 plot2.redraw();
-                //alert('test!');
-
               },
 
               error : function(XMLHttpRequest, textStatus, errorThrown) { console.log('Error!'); } 
